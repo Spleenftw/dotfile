ac699adb0f785186a99541480e14ca71493d39c1
spleenftw <spleenftw@gmail.com>
2022-02-08 09:52:12 +0100

    februar 2022 update

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+set -g -x fish_greeting ''
+
+starship init fish | source
+
+export STARSHIP_CONFIG=/home/spleenftw/.config/starship/starship.toml
+
+source ~/.bash_aliases
+
+
+set fish_color_normal white
+set fish_color_autosuggestion grey
+set fish_color_command white
+set fish_color_param brblue
+set fish_color_redirection green
